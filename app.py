70861a5df106890b29b58f600bb1d0d197a83b6f
InyongHwang <hiy1934@gmail.com>
2023-03-28 14:07:44 +0900

    회원가입 기능 구현

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import hashlib
 from flask import Flask, render_template, request, jsonify, redirect, url_for
 app = Flask(__name__)
 
@@ -9,18 +10,29 @@ db = client.dbsparta
 def home():
     return render_template('index.html')
 
-@app.route('/members/signup_page', methods=["GET"])
-def signup_page():
+@app.route('/members/signup', methods=["GET"])
+def signup_get():
     return render_template('signup.html')
 
 @app.route('/members/signup', methods=["POST"])
-def signup():
+def signup_post():
     email_receive = request.form['email_give']
-    ninkname_receive = request.form['ninkname_give']
+    nickname_receive = request.form['nickname_give']
     password_receive = request.form['password_give']
     bloglink_receive = request.form['bloglink_give']
-    print(email_receive, ninkname_receive, password_receive, bloglink_receive)
-    return redirect("http://localhost:5000/")
+
+    pw_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    
+    doc = {
+        'email': email_receive,
+        'nickname': nickname_receive,
+        'password': pw_hash,
+        'bloglink': bloglink_receive
+    }
+    
+    db.users.insert_one(doc)
+
+    return jsonify({'result':True})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
